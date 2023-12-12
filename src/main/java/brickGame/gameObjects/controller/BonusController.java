958e3b37494947c80f407bba806c71b31cbdcbf6
farah-qistina <farahqistinaalnizam@gmail.com>
2023-12-12 17:12:43 +0800

    added javadoc to BonusController

--- src/main/java/brickGame/gameObjects/controller/BonusController.java
+++ src/main/java/brickGame/gameObjects/controller/BonusController.java
@@ -6,18 +6,30 @@ import brickGame.gameObjects.view.BonusView;
 import javafx.scene.image.Image;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * class for the bonus object
+ */
 public class BonusController {
     private Rectangle bonusFace;
     private BonusModel bonusModel;
     private BonusView bonusView;
 
 
+    /**
+     * constructor for the bonus object
+     * @param xBlock the top left x coordinate of the block that the bonus object is coming from
+     * @param yBlock the top left y coordinate of the block that the bonus object is coming from
+     */
     public BonusController(double xBlock, double yBlock) {
         bonusModel = new BonusModel(xBlock, yBlock);
         bonusFace = makeBonusFace();
         bonusView = new BonusView();
     }
 
+    /**
+     * makes a rectangle bonus face
+     * @return the bonus face
+     */
     private Rectangle makeBonusFace() {
         Rectangle out = new Rectangle();
         out.setWidth(BonusModel.getBonusWidth());
@@ -27,13 +39,24 @@ public class BonusController {
         return out;
     }
 
+    /**
+     * moves the bonus object face vertically
+     * @param y y coordinate of new position
+     */
     public void moveY(double y) {
         bonusFace.setY(y);
     }
 
+    /**
+     * updates the view of the bonus object
+     * @param bonus the bonus object
+     */
     public void updateView(BonusController bonus) {
         bonusView.drawBonus(bonus);
     }
+
+
+    //setter and getter methods
 
     public Image getImage() {return BonusModel.getImage();}
     public void setxBonus(double xBonus) {
